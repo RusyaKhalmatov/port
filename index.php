70ad672da5edaf936d365cc4f91997d6cff199a6
RusyaKhalmatov <rusya.khalmatov@gmail.com>
2017-07-30 23:35:43 +0500

    main added

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
-<!doctype html>
+﻿<!doctype html>
 <html lang="ru">
 <head>
-<title>Bright Ideas | Главная</title>
+<title>Главная</title>
   <meta charset = "utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
